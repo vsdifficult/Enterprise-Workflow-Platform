72d5cd3aff581c5ae3fe7d5c4d3db63c10dc2384
vsdifficult <newfakes46578@gmail.com>
2026-01-10 16:12:53 +0300

    features: add logger in user repository

--- src/auth.service/auth.infrastructure/Repositories/UserRepository.cs
+++ src/auth.service/auth.infrastructure/Repositories/UserRepository.cs
@@ -4,6 +4,7 @@ using auth.infrastructure.entites;
 using auth.shared.dtos;
 using auth.shared.enums;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using shared.repositories;
 
 namespace auth.infrastructure.repositories; 
@@ -11,75 +12,137 @@ namespace auth.infrastructure.repositories;
 public class UserRepository: IUserRepository
 {
     private readonly AuthDbContext _context;
-    public UserRepository(AuthDbContext context)
+
+    private readonly Logger<UserRepository> _logger; 
+    public UserRepository(
+        AuthDbContext context,
+        Logger<UserRepository> logger)
     {
         _context = context; 
+        _logger = logger;
     } 
 
     public async Task<Guid> CreateAsync(UserDto entity)
     {
-        await _context.Users.AddAsync(MapToEntity(entity)); 
-        await _context.SaveChangesAsync(); 
-        return entity.Id; 
+        try
+        {
+            await _context.Users.AddAsync(MapToEntity(entity)); 
+            await _context.SaveChangesAsync(); 
+            return entity.Id;  
+        } 
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error Create user"); 
+            throw; 
+        }
     } 
 
     public async Task<Role?> GetUserRoleAsync(Guid userId)
     {
-        var user = await _context.Users.FindAsync(userId);
-        return user?.UserRole;
+        try 
+        {    
+            var user = await _context.Users.FindAsync(userId);
+            return user?.UserRole;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Error get user {userId}"); 
+            throw; 
+        }
     }
     public async Task<bool> DeleteAsync(Guid id)
     {
-        var user = await _context.Users.FindAsync(id);
-        if (user == null) return false;
-        _context.Users.Remove(user);
-        await _context.SaveChangesAsync();
-        return true;
+        try
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null) return false;
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return true; 
+        } 
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Error delete user {id}"); 
+            throw; 
+        }
     } 
     public async Task<UserDto?> GetByIdAsync(Guid id)
     {
-        var user = await _context.Users.FindAsync(id) 
-            ?? throw new Exception($"User with id {id} not found");
-        return MapToDto(user);
+        try
+        {
+            var user = await _context.Users.FindAsync(id) 
+                ?? throw new Exception($"User with id {id} not found");
+            return MapToDto(user); 
+        } 
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Error get user {id}"); 
+            throw; 
+        }
     } 
 
     public async Task<UserDto?> GetByEmailAsync(string email)
     {
-        var usr = await _context.Users
-            .AsNoTracking()
-            .FirstOrDefaultAsync(u => u.Email == email) 
-                ?? throw new Exception($"User with {email} email not found"); 
-        
-        return MapToDto(usr); 
+        try 
+        {   
+            var usr = await _context.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.Email == email) 
+                    ?? throw new Exception($"User with {email} email not found"); 
+            
+            return MapToDto(usr);  
+        } 
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Error get user with email {email}"); 
+            throw; 
+        }
 
     }
 
     public async Task<bool> SetEmailVerifiedAsync(string email)
     {
-        var usr = await _context.Users
-            .AsNoTracking()
-            .FirstOrDefaultAsync(u => u.Email == email)
-            ?? throw new Exception($"User with {email} email not found"); 
-        
-        usr.IsVerify = true; 
+        try
+        {
+            var usr = await _context.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.Email == email)
+                ?? throw new Exception($"User with {email} email not found"); 
+            
+            usr.IsVerify = true; 
 
-        _context.Update(usr); 
-        await _context.SaveChangesAsync(); 
-        return true; 
+            _context.Update(usr); 
+            await _context.SaveChangesAsync(); 
+            return true; 
+        } 
+        
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Error verify user with email {email}"); 
+            throw; 
+        }
     } 
 
     public async Task<bool> SetVerificationCodeAsync(string email, string code)
     {
-        var usr = await _context.Users
-            .AsNoTracking()
-            .FirstOrDefaultAsync(u => u.Email == email)
-            ?? throw new Exception($"User with {email} email not found");  
+        try 
+        {
+            var usr = await _context.Users
+                .AsNoTracking()
+                .FirstOrDefaultAsync(u => u.Email == email)
+                ?? throw new Exception($"User with {email} email not found");  
 
-        usr.Code = code; 
+            usr.Code = code; 
 
-        _context.Update(usr); 
-        await _context.SaveChangesAsync(); 
-        return true; 
+            _context.Update(usr); 
+            await _context.SaveChangesAsync(); 
+            return true;  
+        } 
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Error set code in user profile with email {email}"); 
+            throw; 
+        }
     }
     public async Task<IEnumerable<UserDto>> GetAllAsync()
     {
