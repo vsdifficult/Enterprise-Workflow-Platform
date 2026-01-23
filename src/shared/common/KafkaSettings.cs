0104fff5613da682569f515acaf2482aefb85516
vsdifficult <newfakes46578@gmail.com>
2026-01-23 19:12:31 +0300

    common add events

---
+++ src/shared/common/KafkaSettings.cs
@@ -0,0 +1,12 @@
+
+namespace shared.common.kafka; 
+public class KafkaSettings
+{
+    public string Host {get; set; }
+
+    public string Topic { get; set; } 
+
+    public string User { get; set; } 
+
+    public string Password { get; set; }
+}
